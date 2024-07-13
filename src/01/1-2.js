87a50d38aad7cc3af488a4b60769b96808d36e1c
tsuchiya-d <tsuchiya-d@aidemy.co.jp>
2024-07-13 14:51:00 +0900

    refactor: ループの分離

--- src/01/1-2.js
+++ src/01/1-2.js
@@ -4,13 +4,15 @@ export function statement(invoice, plays) {
   let result = `Statement for ${invoice.customer}\n`;
 
   for (let perf of invoice.performances) {
-    // ボリューム特典のポイントを加算
-    volumeCredits += volumeCreditsFor(perf);
     // 注文の内訳を出力
     result += ` ${playFor(perf).name}: ${usd(amountFor(perf) / 100)} (${
       perf.audience
     } seats)\n`;
     totalAmount += amountFor(perf);
+  }
+  for (let perf of invoice.performances) {
+    // ボリューム特典のポイントを加算
+    volumeCredits += volumeCreditsFor(perf);
   }
   result += `Amount owed is ${usd(totalAmount / 100)}\n`;
   result += `You earned ${volumeCredits} credits\n`;
