52c4c031e0b769ba7800a68c8b3bd10a34acca29
tsuchiya-d <tsuchiya-d@aidemy.co.jp>
2024-07-13 14:05:06 +0900

    refactor: 右辺値を関数に置き換え

--- src/01/1-2.js
+++ src/01/1-2.js
@@ -25,13 +25,17 @@ export function statement(invoice, plays) {
   let volumeCredits = 0;
   let result = `Statement for ${invoice.customer}\n`;
 
+  function playFor(aPerformance) {
+    return plays[aPerformance.playID];
+  }
+
   const format = new Intl.NumberFormat("en-US", {
     style: "currency",
     currency: "USD",
     minimumFractionDigits: 2,
   }).format;
   for (let perf of invoice.performances) {
-    const play = plays[perf.playID];
+    const play = playFor(perf);
     let thisAmount = amountFor(perf, play);
     // ボリューム特典のポイントを加算
     volumeCredits += Math.max(perf.audience - 30, 0);
