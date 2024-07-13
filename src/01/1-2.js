f89dc35e2c7337e869ec51d16475b99b057bfeb6
tsuchiya-d <tsuchiya-d@aidemy.co.jp>
2024-07-13 14:36:33 +0900

    refactor: 更新されるアキュムレータを関数化

--- src/01/1-2.js
+++ src/01/1-2.js
@@ -10,10 +10,7 @@ export function statement(invoice, plays) {
   }).format;
   for (let perf of invoice.performances) {
     // ボリューム特典のポイントを加算
-    volumeCredits += Math.max(perf.audience - 30, 0);
-    // 喜劇のときは10人につき、さらにポイントを加算
-    if ("comedy" === playFor(perf).type)
-      volumeCredits += Math.floor(perf.audience / 5);
+    volumeCredits += volumeCreditsFor(perf);
     // 注文の内訳を出力
     result += ` ${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${
       perf.audience
@@ -48,5 +45,14 @@ export function statement(invoice, plays) {
         throw new Error(`unknown type: ${playFor(aPerformance).type}`);
     }
     return result;
+  }
+
+  function volumeCreditsFor(perf) {
+    let volumeCredits = 0;
+    volumeCredits += Math.max(perf.audience - 30, 0);
+    if ("comedy" === playFor(perf).type)
+      volumeCredits += Math.floor(perf.audience / 5);
+
+    return volumeCredits;
   }
 }
