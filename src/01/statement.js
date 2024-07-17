20866ab27a0d0a364656eb05c1d797a0793f1a1a
tsuchiya-d <tsuchiya-d@aidemy.co.jp>
2024-07-17 21:29:34 +0900

    refactor: html用の出力処理を追加

--- src/01/statement.js
+++ src/01/statement.js
@@ -4,6 +4,10 @@ export function statement(invoice, plays) {
   return renderPlainText(createStatementData(invoice, plays));
 }
 
+export function htmlStatement(invoice, plays) {
+  return renderHtml(createStatementData(invoice, plays));
+}
+
 function renderPlainText(data) {
   let result = `Statement for ${data.customer}\n`;
 
@@ -17,12 +21,29 @@ function renderPlainText(data) {
   result += `Amount owed is ${usd(data.totalAmount / 100)}\n`;
   result += `You earned ${data.totalVolumeCredits} credits\n`;
   return result;
+}
 
-  function usd(aNumber) {
-    return new Intl.NumberFormat("en-US", {
-      style: "currency",
-      currency: "USD",
-      minimumFractionDigits: 2,
-    }).format(aNumber);
+function renderHtml(data) {
+  let result = `<h1>Statement for ${data.customer}</h1>\n`;
+  result += "<table>\n";
+  result += "<tr><th>play</th><th>seats</th><th>cost</th></tr>";
+
+  for (let perf of data.performances) {
+    result += `<tr><td>${perf.play.name}</td><td>${perf.audience}</td>`;
+    result += `<td>${usd(perf.amount)}</td></tr>`;
   }
+
+  result += "</table>\n";
+  result += `<p>Amount owed is <em>${usd(data.totalAmount)}</em></p>\n`;
+  result += `<p>You earned <em>${data.totalVolumeCredits}</em> credits</p>\n`;
+
+  return result;
+}
+
+function usd(aNumber) {
+  return new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 2,
+  }).format(aNumber);
 }
--- src/01/statement.test.js
+++ src/01/statement.test.js
@@ -1,4 +1,5 @@
-import { statement } from "./statement";
+import { it } from "vitest";
+import { htmlStatement, statement } from "./statement";
 
 it("should return statement result", () => {
   const invoices = {
@@ -32,6 +33,45 @@ it("should return statement result", () => {
     You earned 47 credits
     `;
   const result = statement(invoices, plays);
+
+  // 正規表現を使用して空白を削除
+  const cleanResult = result.replace(/\s/g, "");
+  const cleanWant = want.replace(/\s/g, "");
+  expect(cleanResult.trim()).toEqual(cleanWant);
+});
+
+it("should return html statement result", () => {
+  const invoices = {
+    customer: "BigCo",
+    performances: [
+      {
+        playID: "hamlet",
+        audience: 55,
+      },
+      {
+        playID: "as-like",
+        audience: 35,
+      },
+      {
+        playID: "othello",
+        audience: 40,
+      },
+    ],
+  };
+  const plays = {
+    hamlet: { name: "Hamlet", type: "tragedy" },
+    "as-like": { name: "As You Like It", type: "comedy" },
+    othello: { name: "Othello", type: "tragedy" },
+  };
+  const want = `
+    <h1>Statement for BigCo</h1>
+    <table>
+    <tr><th>play</th><th>seats</th><th>cost</th></tr><tr><td>Hamlet</td><td>55</td><td>$65,000.00</td></tr><tr><td>As You Like It</td><td>35</td><td>$58,000.00</td></tr><tr><td>Othello</td><td>40</td><td>$50,000.00</td></tr></table>
+    <p>Amount owed is <em>$173,000.00</em></p>
+    <p>You earned <em>47</em> credits</p>
+    `;
+  const result = htmlStatement(invoices, plays);
+  console.log(result);
 
   // 正規表現を使用して空白を削除
   const cleanResult = result.replace(/\s/g, "");
